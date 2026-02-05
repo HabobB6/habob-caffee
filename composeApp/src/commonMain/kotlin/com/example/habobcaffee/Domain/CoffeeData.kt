23eb8a96679a3ba9ab784356412131e953e2efe2
Gseeker-My <69015404+Gseeker-My@users.noreply.github.com>
2026-02-05 23:13:11 +0100

    mohammed Review 1

--- composeApp/src/commonMain/kotlin/com/example/habobcaffee/Domain/CoffeeData.kt
+++ composeApp/src/commonMain/kotlin/com/example/habobcaffee/Domain/CoffeeData.kt
@@ -1,12 +1,24 @@
 package com.example.habobcaffee.Domain
 
 data class CoffeeData(
-    val Coffee_ID : String,
-    val Coffee_Name : String,
-    val Coffee_Type : String,
-    val Coffee_Price : String,
-    val Coffee_Description : String?,
-    val Coffee_Rating : String?,
-    val Coffee_ImageURL : List<String>,
-    val Coffee_Quantity : String?
+    val Coffee_ID : Int?= null,
+    val Coffee_Name : String="",
+    val Coffee_Type : CoffeeType?=null,
+    val Coffee_Price : Double=0.0,
+    val Coffee_Description : String="",
+    val Coffee_Rating : Float=0f,
+    val Coffee_ImageURL :  String="",
+    val Coffee_Quantity : Int=0,
+    val Perches_Count: Long=0
     )
+
+
+
+
+enum class CoffeeType{
+    NOIR,CREAM,TEE,SHIH
+}
+
+enum class CoffeErrosType{
+    NO_CONNECTION,TIMEOUT,ORDER_DIDNT_PASS
+}
--- composeApp/src/commonMain/kotlin/com/example/habobcaffee/Presentation/Coffee_List/Coffee_List_State.kt
+++ composeApp/src/commonMain/kotlin/com/example/habobcaffee/Presentation/Coffee_List/Coffee_List_State.kt
@@ -1,14 +1,14 @@
 package com.example.habobcaffee.Presentation.Coffee_List
 
+import com.example.habobcaffee.Domain.CoffeErrosType
 import com.example.habobcaffee.Domain.CoffeeData
 
 data class Coffee_List_State(
-    val Coffee_Error : String? = null,
     val Coffee_Loading : Boolean = false,
     val Coffee_List : List<CoffeeData> = emptyList(),
-    val Coffee_Selected : CoffeeData? = null,
-    val SearchQuery : String = "Dark Coffee",
+    val Coffee_Selected : Int? = null,
+    val SearchQuery : String = "",
     val SearchResults : List<CoffeeData> = emptyList(),
-    val FavoriteCoffees : List<CoffeeData> = emptyList(),
-    val SelectedTabIndex: Int = 0
+    val FavoriteCoffees : List<Int> = emptyList(), // Coffe data Ids
+
 )
